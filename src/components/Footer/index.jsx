01438e9be9377e1354f8fa134dc40f6b04944e6e
Kye <NovusAa12@proton.me>
2024-03-22 17:45:03 +0000

    add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer__container card">
+      <section className="footer">
+        <span>&copy; 2024 Kye Yee</span>
+        <span>All rights reserved &reg;</span>
+      </section>
+    </footer>
+  );
+}
