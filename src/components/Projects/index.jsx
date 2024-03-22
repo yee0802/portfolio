48e189f5e1386e6fc77e61b3ea8e0860545bf6a0
Kye <NovusAa12@proton.me>
2024-03-22 17:34:33 +0000

    add Projects component

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="my-projects card">
+      <h1>My Projects</h1>
+
+      <section className="my-projects__gallery"></section>
+    </section>
+  );
+}
