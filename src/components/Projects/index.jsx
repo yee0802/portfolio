45738aed6d7fc26379c424f5c63fc1704212ecb5
Kye <NovusAa12@proton.me>
2024-03-22 17:41:26 +0000

    add carousel function to Projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
 import "./index.css";
 
 export default function Projects() {
+  const [activeProject, setActiveProject] = useState(1);
+
+  useEffect(() => {
+    const projects = document.querySelectorAll(
+      ".my-projects__gallery__list__item"
+    );
+
+    const carousel = () => {
+      let stt = 0;
+      projects[activeProject].style.transform = "none";
+      projects[activeProject].style.zIndex = 1;
+      projects[activeProject].style.filter = "none";
+      projects[activeProject].style.opacity = 1;
+
+      for (let i = activeProject + 1; i < projects.length; i++) {
+        stt++;
+        projects[i].style.transform = `translateX(${120 * stt}px) scale(${
+          1 - 0.2 * stt
+        }) perspective(16px)`;
+        projects[i].style.zIndex = -stt;
+        projects[i].style.filter = "blur(5px)";
+        projects[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+
+      stt = 0;
+      for (let i = activeProject - 1; i >= 0; i--) {
+        stt++;
+        projects[i].style.transform = `translateX(${-120 * stt}px) scale(${
+          1 - 0.2 * stt
+        }) perspective(16px)`;
+        projects[i].style.zIndex = -stt;
+        projects[i].style.filter = "blur(5px)";
+        projects[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+    };
+
+    carousel();
+  }, [activeProject]);
+
   return (
     <section id="projects" className="my-projects card">
       <h1>My Projects</h1>
