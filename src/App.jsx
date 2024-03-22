2c742499340c927626f705ea29a34a64c99ea457
Kye <NovusAa12@proton.me>
2024-03-22 15:40:40 +0000

    add AboutMe to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+
+      <main>
+        <AboutMe />
+      </main>
     </>
   );
 }
