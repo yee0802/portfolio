28b6017ee0685d436e9b005a9069ad6ff392a082
Kye <NovusAa12@proton.me>
2024-03-25 17:11:07 +0000

    add blurry loading to AboutMe image

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,10 +1,26 @@
 import hero from "../../assets/hero.png";
 import "./index.css";
 
+const loadImage = () => {
+  const blurImage = document.querySelector(".about-me__image");
+
+  if (blurImage.complete) {
+    blurImage.classList.add("loaded");
+  }
+};
+
 export default function AboutMe() {
   return (
     <section className="about-me card">
-      <img src={hero} alt="close up of a monitor with code" />
+      <section className="about-me__image__container">
+        <img
+          src={hero}
+          alt="close up of a monitor with code"
+          className="about-me__image"
+          loading="lazy"
+          onLoad={loadImage}
+        />
+      </section>
 
       <article className="about-me__info__container">
         <h1>About Me</h1>
@@ -18,7 +34,9 @@ export default function AboutMe() {
 
         <div className="resume__box">
           <p>For more details</p>
-          <button>Download CV</button>
+          <a href={hero} download="hero.png" className="resume__box-button">
+            <button>Download CV</button>
+          </a>
         </div>
       </article>
     </section>
